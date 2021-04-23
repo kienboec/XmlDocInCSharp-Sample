e5e0c74bc54ccbb6a9f93d94f5770baeff30e6fb
kienboeck <kienboec@technikum-wien.at>
2021-04-23 18:46:45 +0200

    generating doxygen docs

---
+++ XmlDocInCSharp-Sample/Logic.cs
@@ -0,0 +1,33 @@
+﻿// <copyright file="Logic.cs" company="KIENBOECK">
+// Copyright (c) KIENBOECK. All rights reserved.
+// </copyright>
+// <author>Daniel Kienboeck</author>
+
+namespace XmlDocInCSharp_Sample
+{
+    /// <summary>
+    /// Here is the class for the logic.
+    /// </summary>
+    public class Logic
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether we should force the magic.
+        /// </summary>
+        public bool ForceTheMagic { get; set; } = true;
+
+        /// <summary>
+        /// In "do the logic" the mystic input is converted to pure magic.
+        /// </summary>
+        /// <param name="input">Some mystic input.</param>
+        /// <returns>Here the magic becomes true (plain text).</returns>
+        public string DoTheLogic(string input)
+        {
+            if (this.ForceTheMagic)
+            {
+                return "magic";
+            }
+
+            return $"simsalabim only ({input})";
+        }
+    }
+}
---
+++ XmlDocInCSharp-Sample/Program.cs
@@ -0,0 +1,28 @@
+﻿// <copyright file="Program.cs" company="KIENBOECK">
+// Copyright (c) KIENBOECK. All rights reserved.
+// </copyright>
+// <author>Daniel Kienboeck</author>
+namespace XmlDocInCSharp_Sample
+{
+    using System;
+
+    /// <summary>
+    /// Class containing the entry point and starting the logic.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// The entry point.
+        /// </summary>
+        /// <param name="args">Arguments from command line.</param>
+        public static void Main(string[] args)
+        {
+            // https://www.doxygen.nl/manual/starting.html
+            // doxygen -g <config-file>
+            // doxygen <config-file>
+            // -----------------------------------------------
+            // https://github.com/DotNetAnalyzers/StyleCopAnalyzers/blob/master/documentation/EnableConfiguration.md
+            Console.WriteLine(new Logic().DoTheLogic("mystic input"));
+        }
+    }
+}
